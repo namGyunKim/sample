1528c3a4ac707f4a8442a5ce02152ed374372aa8
namgyunKim <skarbs01@naver.com>
2024-06-20 11:37:04 +0900

    관리자 목록 반환 시 Response 필드 영역 구분 추가

--- src/main/java/gyun/sample/domain/member/payload/response/admin/AllMemberResponse.java
+++ src/main/java/gyun/sample/domain/member/payload/response/admin/AllMemberResponse.java
@@ -1,25 +1,17 @@
 package gyun.sample.domain.member.payload.response.admin;
 
-import gyun.sample.domain.account.enums.AccountRole;
 import gyun.sample.domain.member.entity.Member;
-import gyun.sample.domain.member.enums.MemberType;
+import gyun.sample.domain.member.payload.dto.AllMemberProfileDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class AllMemberResponse {
-    private Long id;
-    private String loginId;
-    private String nickName;
-    private AccountRole role;
-    private MemberType memberType;
 
-    public AllMemberResponse (Member member){
-        this.id = member.getId();
-        this.loginId = member.getLoginId();
-        this.nickName = member.getNickName();
-        this.role = member.getRole();
-        this.memberType = member.getMemberType();
+    private AllMemberProfileDTO profile;
+
+    public AllMemberResponse(Member member) {
+        this.profile = new AllMemberProfileDTO(member);
     }
 }
