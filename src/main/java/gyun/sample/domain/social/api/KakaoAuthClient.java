11b02b69e238ad95c48739885326d522dea68b71
namgyunkim <skarbs01@naver.com>
2024-07-26 17:44:18 +0900

    Import Optimize

--- src/main/java/gyun/sample/domain/social/api/KakaoAuthClient.java
+++ src/main/java/gyun/sample/domain/social/api/KakaoAuthClient.java
@@ -3,7 +3,6 @@ package gyun.sample.domain.social.api;
 import gyun.sample.domain.social.payload.request.KakaoTokenRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
