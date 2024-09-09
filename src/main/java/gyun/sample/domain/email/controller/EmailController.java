8484a403b22cd694e8726baea2716a583bc636a2
namgyunkim <skarbs01@naver.com>
2024-09-10 00:26:35 +0900

    메일 기능 추가

---
+++ src/main/java/gyun/sample/domain/email/controller/EmailController.java
@@ -0,0 +1,32 @@
+package gyun.sample.domain.email.controller;
+
+import gyun.sample.domain.email.service.EmailService;
+import gyun.sample.global.api.RestApiController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+@RequiredArgsConstructor
+@Tag(name = "EmailController", description = "이메일 관련 api")
+public class EmailController {
+
+    private final EmailService emailService;
+    private final RestApiController restApiController;
+
+    @PostMapping("/send")
+    @Operation(summary = "이메일 보내기")
+    public ResponseEntity<String> sendEmail(
+            @RequestParam String to,
+            @RequestParam String subject,
+            @RequestParam String text) {
+        emailService.sendEmail(to, subject, text);
+        return restApiController.createSuccessRestResponse("Email sent successfully");
+    }
+}
---
+++ src/main/java/gyun/sample/domain/email/service/EmailService.java
@@ -0,0 +1,26 @@
+package gyun.sample.domain.email.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        message.setFrom(from);  // 보내는 사람 이메일
+
+        mailSender.send(message);
+    }
+}
