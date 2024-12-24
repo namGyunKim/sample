9cc86687ae8b99f1c06f5c1f2bce605876b985da
namgyunkim <skarbs01@naver.com>
2024-12-24 21:02:03 +0900

    예외처리 관련 수정

--- src/main/java/gyun/sample/global/exception/advice/ControllerExceptionAdvice.java
+++ src/main/java/gyun/sample/global/exception/advice/ControllerExceptionAdvice.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import gyun.sample.domain.account.payload.dto.CurrentAccountDTO;
 import gyun.sample.global.annotaion.CurrentAccount;
 import gyun.sample.global.exception.GlobalException;
-import gyun.sample.global.exception.JWTInterceptorException;
-import gyun.sample.global.exception.SocialException;
 import gyun.sample.global.exception.enums.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataAccessException;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,70 +18,46 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import java.nio.file.AccessDeniedException;
-
 
 // 예외 처리
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class ControllerExceptionAdvice extends RestApiControllerAdvice {
 
-
     public ControllerExceptionAdvice(ObjectMapper objectMapper, ApplicationEventPublisher applicationEventPublisher) {
         super(objectMapper, applicationEventPublisher);
     }
 
-    // Global Exception Catch
     @ExceptionHandler(value = GlobalException.class)
-    protected ResponseEntity<String> processCommonException(GlobalException commonException, @CurrentAccount CurrentAccountDTO account, HttpServletRequest httpServletRequest) {
-        ErrorCode errorCode = commonException.getErrorCode();
-        // Event - Log
-        sendLogEvent(commonException, account, httpServletRequest);
-        return createFailRestResponse(errorCode.getErrorResponse());
+    public String handleException(GlobalException exception, Model model, HttpServletRequest request, @CurrentAccount CurrentAccountDTO currentAccountDTO) {
+        ErrorCode errorCode = getErrorCodeByException(exception);
+
+        // 에러 정보를 모델에 추가
+        model.addAttribute("errorCode", errorCode.getCode());
+        model.addAttribute("errorMessage", errorCode.getErrorMessage());
+        model.addAttribute("path", request.getRequestURI());
+        sendLogEvent(exception, currentAccountDTO, request);
+
+        return "error/common"; // 공통 에러 페이지로 이동
     }
 
-    // JWT Interceptor Exception Catch
-    @ExceptionHandler(value = JWTInterceptorException.class)
-    protected ResponseEntity<String> processJWTInterceptorException(JWTInterceptorException jwtInterceptorException, @CurrentAccount CurrentAccountDTO account, HttpServletRequest httpServletRequest) {
-        ErrorCode errorCode = jwtInterceptorException.getErrorCode();
-        // Event - Log
-        sendLogEvent(jwtInterceptorException, account, httpServletRequest);
-        return createFailRestResponseWithJWT(errorCode.getErrorResponse());
-    }
-
-    // Social Exception Catch
-    @ExceptionHandler(value = SocialException.class)
-    protected ResponseEntity<String> processSocialException(SocialException socialException, @CurrentAccount CurrentAccountDTO account, HttpServletRequest httpServletRequest) {
-        ErrorCode errorCode = socialException.getErrorCode();
-        // Event - Log
-        sendLogEvent(socialException, account, httpServletRequest);
-        return createFailRestResponse(errorCode.getErrorResponseWithSocial(socialException.getErrorDetailMessage()));
-    }
-
-    @ExceptionHandler(value = Exception.class)
-    protected ResponseEntity<String> processException(Exception exception, @CurrentAccount CurrentAccountDTO account, HttpServletRequest httpServletRequest) {
-        ErrorCode errorCode;
-        if (exception instanceof DataAccessException) {
-            errorCode = ErrorCode.DATA_ACCESS_ERROR;
-        } else if (exception instanceof AccessDeniedException) {
-            errorCode = ErrorCode.ACCESS_DENIED;
+    private ErrorCode getErrorCodeByException(Exception exception) {
+        if (exception instanceof GlobalException) {
+            return ((GlobalException) exception).getErrorCode();
+        } else if (exception instanceof DataAccessException) {
+            return ErrorCode.DATA_ACCESS_ERROR;
         } else if (exception instanceof MaxUploadSizeExceededException) {
-            errorCode = ErrorCode.MAX_UPLOAD_SIZE_EXCEEDED;
+            return ErrorCode.MAX_UPLOAD_SIZE_EXCEEDED;
         } else if (exception instanceof MethodArgumentNotValidException) {
-            errorCode = ErrorCode.INPUT_VALUE_INVALID;
+            return ErrorCode.INPUT_VALUE_INVALID;
         } else if (exception instanceof NoHandlerFoundException) {
-            errorCode = ErrorCode.PAGE_NOT_EXIST;
+            return ErrorCode.PAGE_NOT_EXIST;
         } else if (exception instanceof HttpRequestMethodNotSupportedException) {
-            errorCode = ErrorCode.METHOD_NOT_SUPPORTED;
+            return ErrorCode.METHOD_NOT_SUPPORTED;
         } else if (exception instanceof IllegalArgumentException) {
-            errorCode = ErrorCode.ARGUMENT_INVALID;
+            return ErrorCode.ARGUMENT_INVALID;
         } else {
-            errorCode = ErrorCode.FAILED;
+            return ErrorCode.FAILED; // 기본 에러 코드
         }
-
-        GlobalException globalException = new GlobalException(errorCode, exception);
-        // Event - Log
-        sendLogEvent(globalException, account, httpServletRequest);
-        return createFailRestResponseWithJWT(errorCode.getErrorResponse());
     }
 }
