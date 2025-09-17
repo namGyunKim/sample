d3441c76e505512067224f0f1ff122d28200388f
namgyunkim <skarbs01@naver.com>
2025-09-17 10:45:48 +0900

    예외 처리 수정

--- src/main/java/gyun/sample/global/exception/GlobalException.java
+++ src/main/java/gyun/sample/global/exception/GlobalException.java
@@ -3,36 +3,35 @@ package gyun.sample.global.exception;
 import gyun.sample.global.exception.enums.ErrorCode;
 import lombok.Getter;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 // 전역 예외 처리
 @Getter
 public class GlobalException extends RuntimeException {
 
     private final ErrorCode errorCode;
-    private final String errorDetailMessage;
 
-
+    /**
+     * ErrorCode만으로 예외를 생성합니다.
+     * 메시지는 ErrorCode의 기본 메시지를 사용합니다.
+     */
     public GlobalException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage()); // 부모 클래스에 에러 메시지 전달
         this.errorCode = errorCode;
-        this.errorDetailMessage = getStackTraceMessage(this);
     }
 
-    public GlobalException(ErrorCode errorCode, String errorDetailMessage) {
+    /**
+     * ErrorCode와 함께 별도의 상세 메시지를 지정합니다.
+     */
+    public GlobalException(ErrorCode errorCode, String detailMessage) {
+        super(detailMessage); // 부모 클래스에 상세 메시지 전달
         this.errorCode = errorCode;
-        this.errorDetailMessage = errorDetailMessage;
     }
 
-    public GlobalException(ErrorCode errorCode, Exception exception) {
+    /**
+     * 다른 예외(cause)를 감싸서(Wrapping) 새로운 GlobalException으로 만듭니다.
+     * 이 생성자가 가장 중요하며, 예외의 원인을 추적할 수 있게 해줍니다.
+     */
+    public GlobalException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getErrorMessage(), cause); // 부모 클래스에 메시지와 원인 예외를 함께 전달
         this.errorCode = errorCode;
-        this.errorDetailMessage = getStackTraceMessage(exception);
     }
-
-    private String getStackTraceMessage(Exception exception) {
-        StringWriter stringWriter = new StringWriter();
-        exception.printStackTrace(new PrintWriter(stringWriter));
-        return stringWriter.toString();
-    }
-
 }
