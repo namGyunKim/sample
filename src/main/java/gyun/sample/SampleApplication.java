86868c7fb88da1e959d011d93a2d56ae594352dc
namgyunkim <skarbs01@naver.com>
2025-10-29 17:17:10 +0900

    Enable JPA auditing in SampleApplication

--- src/main/java/gyun/sample/SampleApplication.java
+++ src/main/java/gyun/sample/SampleApplication.java
@@ -3,11 +3,13 @@ package gyun.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableAsync
+@EnableJpaAuditing
 public class SampleApplication {
 
 	public static void main(String[] args) {
